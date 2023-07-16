3823d94caca81ea5e67c87e3a1bbc3d290f1fdec
suryacodes <62347567+suryacodes@users.noreply.github.com>
2023-07-16 11:15:01 +0530

    3 files modified, 4 deleted

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -10,9 +10,15 @@ interface IProdcuts {
 
 const Card: React.FC<IProdcuts> = ({ id, name, price, imgUrl }) => {
   return (
-    <Cardbs className="m-16">
+    <Cardbs className="mt-2">
+      <Cardbs.Img src={imgUrl} height="200px" style={{ objectFit: 'cover' }} />
       <Cardbs.Body>
-        <Cardbs.Img src={imgUrl} />
+        <Cardbs.Title className="d-flex justify-content-between align-items-baseline">
+          <span className="fs-3 ml-2" style={{ wordBreak: 'break-all' }}>
+            {name}
+          </span>
+          <span className="fs-6 ms-2 text-muted">{price.toString()}</span>
+        </Cardbs.Title>
       </Cardbs.Body>
     </Cardbs>
   );
--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,14 +1,21 @@
 import * as React from 'react';
-import { Card } from 'react-bootstrap';
+import { Col, Row } from 'react-bootstrap';
 import items from '../data/items.json';
+import { Card } from '../components';
 
 const StorePage: React.FC<{}> = (props) => {
   return (
     <>
       <div>StorePage</div>
-      {items.map((item) => {
-        return <Card item={item} />;
-      })}
+      <Row md={2} xs={1} lg={3} className="g-3">
+        {items.map((item) => {
+          return (
+            <Col>
+              <Card {...item} />
+            </Col>
+          );
+        })}
+      </Row>
     </>
   );
 };
